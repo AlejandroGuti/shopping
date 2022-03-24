68598d9ee57caed3aac415e7008ff92e0f98e39a
ALEJANDRO GR <alegut123@hotmail.com>
2022-03-24 18:54:49 -0500

    Interfaz

--- Shopping/Shopping/Controllers/CountriesController.cs
+++ Shopping/Shopping/Controllers/CountriesController.cs
@@ -21,12 +21,14 @@ namespace Shopping.Controllers
         }
 
         // GET: Countries
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Countries.ToListAsync());
         }
 
         // GET: Countries/Details/5
+        [HttpGet]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,6 +46,7 @@ namespace Shopping.Controllers
             return View(country);
         }
 
+        [HttpGet]
         // GET: Countries/Create
         public IActionResult Create()
         {
@@ -53,9 +56,10 @@ namespace Shopping.Controllers
         // POST: Countries/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Create(Country country)
         {
             if (ModelState.IsValid)
             {
--- Shopping/Shopping/Data/Entities/Country.cs
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -5,9 +5,9 @@ namespace Shopping.Data.Entities
     public class Country
     {
         public int Id { get; set; }
-        [Display(Name ="Pais")]
-        [MaxLength(50, ErrorMessage ="El campo {0} debe tener maximo {1} caracteres.")]
-        [Required(ErrorMessage ="Campo {0} es obligatorio.")]
+        [Display(Name ="Country")]
+        [MaxLength(50, ErrorMessage ="The field {0} must be max {1} characters.")]
+        [Required(ErrorMessage ="field {0} is mandatory.")]
         public string Name { get; set; }
 
     }
